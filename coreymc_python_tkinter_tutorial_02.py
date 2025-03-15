77bf888d67234e2cbeb4bd497cbc3342d0d89e95
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-03-15 09:53:32 +0200

    Finish functionality

--- coreymc_python_tkinter_tutorial_02.py
+++ coreymc_python_tkinter_tutorial_02.py
@@ -6,20 +6,24 @@ import tkinter as tk
 root = tk.Tk()
 root.title('Simple app')
 
-def add_to_list():
+def add_to_list(event=None):
     text = entry.get()
     if text:
-        text_list.insert(tk.END, text)
+        text_list.insert(tk.END , text)
         entry.delete(0, tk.END)
 
 frame = tk.Frame(root)
 frame.grid(column=0, row=0)
 
 entry = tk.Entry(frame)
-entry.grid(column=0, row=0)
+entry.grid(row=0, column=0)
+entry.bind('<Return>', add_to_list)
+# entry.bind('<Return>', lambda event: add_to_list()) # Alternative with lambda
+
+
 
 entry_btn = tk.Button(frame, text='Add', command=add_to_list)
-entry_btn.grid(column=1, row=0)
+entry_btn.grid(row=0, column=1)
 
 text_list = tk.Listbox(frame)
 text_list.grid(column=0, row=1)
